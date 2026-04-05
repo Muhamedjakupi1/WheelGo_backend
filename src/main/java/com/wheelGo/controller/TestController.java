2e284667f6e3f7b54c7f1b86a056372326dcc2d8
Muhamed Jakupi <muhamedjakupi056@gmail.com>
2026-04-05 23:30:50 +0200

    SCRUM-7: Allow API access from frontend

---
+++ src/main/java/com/wheelGo/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.wheelGo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/test")
+    public Map<String, String> test() {
+        return Map.of("message", "Hello from Spring Boot");
+    }
+}
