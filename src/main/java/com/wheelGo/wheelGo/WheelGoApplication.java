32064640fafeaa7ff4c339f32aa744b28960efaf
Muhamed Jakupi <muhamedjakupi056@gmail.com>
2026-04-05 21:17:58 +0200

    SCRUM 7: Setting up configurations for backend using SpringBoot

---
+++ src/main/java/com/wheelGo/wheelGo/WheelGoApplication.java
@@ -0,0 +1,13 @@
+package com.wheelGo.wheelGo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheelGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WheelGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wheelGo/wheelGo/WheelGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wheelGo.wheelGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheelGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
