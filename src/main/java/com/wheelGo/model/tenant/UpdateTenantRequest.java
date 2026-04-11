649419c86a487286a7d838173ddc40efb26c9ec0
Muhamed Jakupi <muhamedjakupi056@gmail.com>
2026-04-12 01:19:04 +0200

    SCRUM-11_Medi: Added some methods

--- src/main/java/com/wheelGo/model/tenant/UpdateTenantRequest.java
+++ src/main/java/com/wheelGo/model/tenant/UpdateTenantRequest.java
@@ -1,20 +1,11 @@
 package com.wheelGo.model.tenant;
 
-import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
 public class UpdateTenantRequest {
-
-    private String name;
-
-    @Pattern(
-            regexp = "^[a-z0-9-]{2,50}$",
-            message = "Slug: vetëm shkronja të vogla, numra dhe vizë"
-    )
-    private String slug;
-
+    private String  name;
     private String  plan;
     private Boolean isActive;
 }
